6ac3b8128b00d04b6c62992c24e7d49180d91ee8
kapilchhajer <kapil.chhajer@gmail.com>
2015-09-14 17:46:08 +0530

    ticket curration changed

--- src/app/scripts/controllers/ticketListCont.js
+++ src/app/scripts/controllers/ticketListCont.js
@@ -1,12 +1,34 @@
-angular.module('chronos').controller('TicketListCtrl',['$scope', 'ticketFilterService', 'headingService', function ($scope, ticketFilterService, headingService){
+angular.module('chronos').controller('TicketListCtrl',
+	['$scope',
+	'$window',
+	'ticketFilterService',
+	'headingService',
+	'ticketCurationService',
+	function (
+		$scope,
+		$window,
+		ticketFilterService,
+		headingService,
+		ticketCurationService){
   $scope.itemsPerPage = ticketFilterService.itemsPerPage;
   $scope.tickets = ticketFilterService.tickets;
-  headingService.set("Issues List")
+  headingService.set("Issues List");
+
   console.log(headingService.pageHeading)
+
+  $scope.openTicketCuration = function(id) {
+    console.log(id)
+    ticketCurationService.setTicketId(id);
+    console.log(ticketCurationService)
+    console.log(ticketCurationService.getTicketId())
+    console.log("ticket Id set in services");
+    $window.showCuration(1);
+    angular.element(document.getElementById('curation-main')).scope().showTicket();
+  };
 
   $scope.pageChange = function(newPageNumber){
     console.log(newPageNumber)
     offset = (newPageNumber - 1) * $scope.itemsPerPage
     ticketFilterService.update_tickets_with_filters(offset);
   }
-}])
+}]);
---
+++ src/app/scripts/services/ticketCurationService.js
@@ -0,0 +1,21 @@
+angular.module('chronos').factory('ticketCurationService',function(){
+  var ticketId;
+  var showFlag = false;
+  var setTicketId = function(id){
+  	console.log('in',id)
+    this.ticketId = id;
+    this.showFlag = true;
+  };
+  var getTicketId = function() {
+  	return this.ticketId;
+  };
+
+  var resetTicketCuration = function(){
+  	this.showFlag = false;
+  }
+  return {
+    getTicketId : getTicketId,
+    setTicketId : setTicketId,
+    resetTicketCuration : resetTicketCuration
+  }
+});
