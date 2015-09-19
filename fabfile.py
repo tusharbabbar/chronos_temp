22e14138dd1355caee6f69aea2ee0c3e4f522105
TusharBabbar <babbartushar@gmail.com>
2015-09-19 17:50:02 +0530

    Version 0.1 set for deployment

--- fabfile.py
+++ fabfile.py
@@ -7,3 +7,4 @@ def build():
         with cd('./dist/'):
             local("sed -i '' 's/chronos\/static/static/g' dist/*.js")
             local("sed -i '' 's/\.\./\/assets/g' dist/*.css")
+            local("sed -i '' 's/http:\/\/localhost:5000/https:\/\/chronos\.practo\.com/g' dist/*.js")
