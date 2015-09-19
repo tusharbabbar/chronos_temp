ea84fe584ace3ac64ce94e1df116b546c55a17ff
TusharBabbar <babbartushar@gmail.com>
2015-09-19 18:46:01 +0530

    updated nav top

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,6 @@ def build():
     local("gulp dist")
     if os.path.exists('./dist'):
         with cd('./dist/'):
-            local("sed -i '' 's/chronos\/static/static/g' dist/*.js")
-            local("sed -i '' 's/\.\./\/assets/g' dist/*.css")
-            local("sed -i '' 's/http:\/\/localhost:5000/https:\/\/chronos\.practo\.com/g' dist/*.js")
+            local("sed -i -e 's/chronos\/static/static/g' dist/*.js")
+            local("sed -i -e 's/\.\./\/assets/g' dist/*.css")
+            local("sed -i -e 's/http:\/\/localhost:5000/https:\/\/chronos\.practo\.com/g' dist/*.js")
