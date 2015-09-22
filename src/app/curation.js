e6153f28237c1291c9f7d7fce1b07336821c2e50
kapilchhajer <kapil.chhajer@gmail.com>
2015-09-22 14:40:16 +0530

    curation team with_members only

--- src/app/curation.js
+++ src/app/curation.js
@@ -51,11 +51,11 @@ angular.module('chronos').controller('curationCtrl',
               $scope.types = data.types;
             });
             //get list of owner teams
-            TeamsListApi.get( {if_owner : 1}, function (data) {
+            TeamsListApi.get( {if_owner : 1, with_members : 1}, function (data) {
               $scope.ownerTeams = data.teams;
             });
             //get list of assigned teams
-            TeamsListApi.get( {if_assigned : 1}, function (data) {
+            TeamsListApi.get( {if_assigned : 1, with_members : 1}, function (data) {
               $scope.assignedTeams = data.teams;
             });
             $scope.capitalizeFirstLetter = function toTitleCase(string){
