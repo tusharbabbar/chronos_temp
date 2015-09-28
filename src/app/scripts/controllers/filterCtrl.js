e46b95f9387b52eea4e221a5a8ea1b59fbdc2dce
Sandeep Pandey <sandeep.pandey@practo.com>
2015-09-28 11:14:59 +0530

    Added meta service support for left filter pannel

--- src/app/scripts/controllers/filterCtrl.js
+++ src/app/scripts/controllers/filterCtrl.js
@@ -14,43 +14,15 @@ angular.module('chronos').controller('TicketListFilterCtrl',
                                TypesListApi,
                                TicketsListCountApi
                             ){
-  SourcesListApi.get({},function(data){
-    $scope.sources = data.sources
-  })
-  ProductsListApi.get({}, function(data){
-    $scope.products = data.products
-  })
-  TeamsListApi.get({all:1, with_members : 1, limit: 100}, function(data){
-    //console.log(data.teams)
-    $scope.teams = data.teams
-  })
-  TypesListApi.get({}, function(data){
-    $scope.types = data.types
-  })
-  $scope.counts = {}
-  TicketsListCountApi.get({my_issues:1},function(data){
-    $scope.counts["my_issues"] = data.count
-  });
-  TicketsListCountApi.get({all_issues:1},function(data){
-    $scope.counts["all_issues"] = data.count
-  });
-  TicketsListCountApi.get({my_team_issues:1},function(data){
-    $scope.counts["my_team_issues"] = data.count
-  });
-  //console.log($scope.counts)
-  $scope.statuses = [
+  $scope.data = $scope.data ? $scope.data : {};
+  $scope.data.statuses = [
     {name : 'ORPHAN', icon : '<img src="/assets/fonts/Orphan.svg"></img>'},
     {name : 'CURATION', icon : '<img src="/assets/fonts/Curation.svg"></img>'},
     {name : 'RESOLVED', icon : '<img src="/assets/fonts/Resolved.svg"></img>'},
     {name : 'INPORGRESS', icon : '<img src="/assets/fonts/inProgress.svg"></img>'},
     {name : 'INVALID', icon : '<img src="/assets/fonts/Invalid.svg"></img>'}]
 
-  $scope.selected = ticketFilterService.filters
-  //console.log($scope.selected === ticketFilterService.filters)
-  // var items1 = ['product', 'type', 'source', 'owner', 'team', 'status']
-  // for (var i=0;i<items1.length; i++){
-  //   $scope[items1[i]] = {}
-  // }
+  $scope.selected = ticketFilterService.filters;
   $scope.preFilteredIssues = function(_type){
     $scope.selected.my_issues = 0
     $scope.selected.my_team_issues = 0
@@ -68,9 +40,6 @@ angular.module('chronos').controller('TicketListFilterCtrl',
       $scope.selected.my_team_issues = 1
     else if (_type == 'all_issues')
       $scope.selected.all_issues = 1
-    //console.log($scope.selected === ticketFilterService.filters)
-    //console.log($scope.selected)
-    //console.log(ticketFilterService.filters)
     $scope.apply_filters()
   }
 
@@ -79,7 +48,4 @@ angular.module('chronos').controller('TicketListFilterCtrl',
   }
 
   $scope.preFilteredIssues("all_issues");
-  $scope.temp = function(){
-    //console.log($scope)
-  }
 }])
--- src/app/scripts/services/metaService.js
+++ src/app/scripts/services/metaService.js
@@ -1,13 +1,14 @@
 /**
-  Service to set meta info required by the entire Chronos app.
-  Each meta parameter has its own function which can be used any where in the angular module.
-  To refresh all the meta parameters make a call to 'refresh' method.
+Service to set meta info required by the entire Chronos app.
+Each meta parameter has its own function which can be used any where in the angular module.
+To refresh all the meta parameters make a call to 'refresh' method.
 */
 
 angular.module('chronos').factory('Meta',
-function($rootScope, SourcesListApi, ProductsListApi, TypesListApi, TeamsListApi){
+function($rootScope, SourcesListApi, ProductsListApi, TypesListApi, TeamsListApi, TicketsListCountApi){
   var meta = new Object();
   $rootScope.data = $rootScope.data ? $rootScope.data : {};
+  $rootScope.data.counts = {}
 
   meta.refreshSources = function(){
     SourcesListApi.get(function (data) {
@@ -39,11 +40,38 @@ function($rootScope, SourcesListApi, ProductsListApi, TypesListApi, TeamsListApi
     });
   }
 
+  meta.refreshMyIssuesCount = function(){
+    TicketsListCountApi.get({my_issues:1},function(data){
+      $rootScope.data.counts["my_issues"] = data.count ? data.count  : 0;
+    });
+  }
+
+  meta.refreshAllIssuesCount = function(){
+    TicketsListCountApi.get({all_issues:1},function(data){
+      $rootScope.data.counts["all_issues"] = data.count ? data.count : 0;
+    });
+  }
+
+  meta.refreshMyTeamIssueCount = function(){
+    TicketsListCountApi.get({my_team_issues:1},function(data){
+      $rootScope.data.counts["my_team_issues"] = data.count ? data.count : 0;
+    });
+  }
+
   meta.refresh = function(){
-    var metaParams = ['Sources', 'Products', 'Types', 'OwnerTeams', 'AssignedTeams'];
-     metaParams.forEach(function(metaParam){
-        meta['refresh'+metaParam]();
-     })
+    var metaParams = [
+      'Sources',
+      'Products',
+      'Types',
+      'OwnerTeams',
+      'AssignedTeams',
+      'MyIssuesCount',
+      'AllIssuesCount',
+      'MyTeamIssueCount'
+    ];
+    metaParams.forEach(function(metaParam){
+      meta['refresh'+metaParam]();
+    })
   }
 
   return meta;
