27fd11950c054261a762c4aacdecfe7e9ca30707
Sandeep Pandey <sandeep.pandey@practo.com>
2015-09-29 12:36:27 +0530

    Corrected time left display for chronos ticket list

--- src/app/scripts/services/ticketFilterService.js
+++ src/app/scripts/services/ticketFilterService.js
@@ -1,4 +1,4 @@
-angular.module('chronos').factory('ticketFilterService',function(TicketsListApi){
+angular.module('chronos').factory('ticketFilterService',function($rootScope, TicketsListApi){
   var tickets = {};
   tickets.list = [];
   tickets.total = 0;
@@ -13,8 +13,11 @@ angular.module('chronos').factory('ticketFilterService',function(TicketsListApi)
     types : [],
     sources : [],
     statuses : [],
-    offset: 0
-  }
+    offset: 0,
+    itemsPerPage: itemsPerPage
+  };
+
+  var self = this;
 
   var get_tickets = function(){
     //console.log(tickets);
@@ -22,64 +25,48 @@ angular.module('chronos').factory('ticketFilterService',function(TicketsListApi)
   }
   var update_tickets_with_filters = function(){
     var self = this;
+    var filtersAttr = ['product', 'type', 'source', 'team', 'statuse'];
+
     var data = {
       offset : self.filters.offset,
+      limit : self.filters.itemsPerPage,
+      my_issues : self.filters.my_issues,
+      my_team_issues : self.filters.my_team_issues,
+      all_issues : self.filters.all_issues
     };
-    data.limit = self.itemsPerPage;
-    data.my_issues = self.filters.my_issues;
-    data.my_team_issues = self.filters.my_team_issues;
-    data.all_issues = self.filters.all_issues;
-    data.product_id = self.filters.products.map(function(obj){
-      return obj.id
-    });
-    data.type_id = self.filters.types.map(function(obj){
-      return obj.id
-    });
-    data.source_id = self.filters.sources.map(function(obj){
-      return obj.id
-    });
-    data.team_id = self.filters.teams.map(function(obj){
-      return obj.id
+    filtersAttr.forEach(function(filterAttr){
+      data[filterAttr + "_id"] = self.filters[filterAttr + "s"].map(function(obj){
+          return obj.id
+      })
     });
     data.status = self.filters.statuses.map(function(obj){
       return obj.name
     });
+
     TicketsListApi.get(data, function(data){
       now = Date.now()/1000
-      for (i in data.tickets){
-        data.tickets[i]['remaining'] = parseInt(((now - data.tickets[i]['created_on'])/60/60 - data.tickets[i]['sla']))
-        if (data.tickets[i]['remaining'] > 0){
-          var status = "passed"
-        }
-        else {
-          var status = "remains"
-        }
-        data.tickets[i]['remaining'] = Math.abs(data.tickets[i]['remaining'])
-        if (data.tickets[i]['remaining'] >= 24 ){
-          data.tickets[i]['remaining_days'] = parseInt(data.tickets[i]['remaining'] / 24)
-          data.tickets[i]['remaining_hours'] = parseInt(data.tickets[i]['remaining'] % 24)
-        }
-        else {
-          data.tickets[i]['remaining_hours'] = parseInt(data.tickets[i]['remaining'] % 24)
-        }
-        if (data.tickets[i]['remaining_days'] == 1){
-          data.tickets[i]['remaining_days_unit'] = ' day &'
-        }
-        else if (data.tickets[i]['remaining_days'] > 1) {
-          data.tickets[i]['remaining_days_unit'] = ' days &'
-        }
-        if (data.tickets[i]['remaining_hours'] == 1){
-            data.tickets[i]['remaining_hours_unit'] = ' hour ' + status
+      var timeRemainingTicketStatus = ["INPROGRESS", "ORPHAN", "CURATION"];
+      if(data.tickets)
+      data.tickets.forEach(function(ticket){
+        if(timeRemainingTicketStatus.indexOf(ticket.status) > -1){
+          ticket['remaining'] = parseInt(((now - ticket['created_on'])/60/60 - ticket['sla']))
+          if (ticket['remaining'] > 0){
+            var status = "passed"
           }
-        else if (data.tickets[i]['remaining_hours'] > 1){
-          data.tickets[i]['remaining_hours_unit'] = ' hours ' + status
+          else {
+            var status = "remains"
+          }
+          ticket['remaining'] = Math.abs(ticket['remaining']);
+          ticket['remaining_days'] =  ticket['remaining'] >= 24  ? parseInt(ticket['remaining'] / 24) : undefined;
+          ticket['remaining_hours'] = parseInt(ticket['remaining'] % 24);
+          ticket['remaining_days_unit'] = ticket['remaining_days'] == 1 ? ' day &' : ticket['remaining_days'] > 1? ' days &' : undefined;
+          ticket['remaining_hours_unit'] = (ticket['remaining_hours'] == 1 ? ' hour ' : ticket['remaining_hours'] > 1 ? " hours " :  undefined) + status;
+          ticket['remaining_percent'] = parseInt(((now - ticket['created_on'])/60/60 - ticket['sla']) * -1)/ticket['sla']*100
         }
-        data.tickets[i]['remaining_percent'] = parseInt(((now - data.tickets[i]['created_on'])/60/60 - data.tickets[i]['sla']) * -1)/data.tickets[i]['sla']*100
-      }
+      })
       self.tickets.list = data.tickets
       self.tickets.total = data.total
       self.tickets.pages = _.range(window.Math.ceil(data.total/self.itemsPerPage))
-      //console.log(data.tickets)
     })
   }
 
